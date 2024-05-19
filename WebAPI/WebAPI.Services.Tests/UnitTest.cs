bd0364dd437a63e1ed3e359bc9571b182389cede
Charles De-Wind <cdewind@outlook.com>
2024-05-19 16:08:32 +0100

    ci cd pipelines

---
+++ WebAPI/WebAPI.Services.Tests/UnitTest.cs
@@ -0,0 +1,18 @@
+using FluentAssertions;
+
+namespace WebAPI.Services.Tests
+{
+    public class UnitTest
+    {
+        [Theory]
+        [InlineData(1,2,3)]
+        [InlineData(20, 2, 22)]
+        [InlineData(100, 22, 122)]
+        [InlineData(-900,100,-800)]
+        public void TestFunctionForCiCdPipeline(int x, int y, int expected)
+        {
+            var actualSum = x + y;
+            actualSum.Should().Be(expected);
+        }
+    }
+}
